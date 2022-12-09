c55f1b6122bccca67832901412958db7543e8150
jobpatiphan <patiphan_klinhom@cmu.ac.th>
2022-12-09 15:31:20 +0700

    lap6_1.cpp COMPLETE

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 int main(){
+    int a = 1;
+    int b = 0;
+    int c = 0;
+    while(a != 0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> a;
+    if(a%2 == 0){
+        b++;
+    }
+    else{ c++;
+    }
+    }
+    cout << "#Even numbers = " << b-15 << endl;
+
+    cout << "#Odd numbers = " << c;
     return 0;
 }
